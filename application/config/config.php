79605e8c12f22747cf662f36594eb1620244f8aa
Master Yoda <jim_parry@bcit.ca>
2015-03-30 10:22:52 -0700

    Update for 3.0 release Signed-off-by:Master Yoda <jim_parry@bcit.ca>

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ $config['footer_choices'] = array(
     )
 );
 
-$config['stable_version'] = '3.0rc3';
+$config['stable_version'] = '3.0.0';
 
 /*
   |--------------------------------------------------------------------------
