c5d0ddf11301f93f6627cb8cbca7c5c46d952ce1
Karim Baaba <karim.baaba@gmail.com>
2015-09-27 19:17:38 -0600

    Add inline github stats & update butto nstyles

--- application/views/theme/jumbotitle.php
+++ application/views/theme/jumbotitle.php
@@ -12,21 +12,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
             <h1>CodeIgniter Rocks</h1>
-            <p>CodeIgniter is a powerful PHP framework with a very small footprint, built for developers who need 
+            <p>CodeIgniter is a powerful PHP framework with a very small footprint, built for developers who need
                 a simple and elegant toolkit to create full-featured web applications.</p>
-            <div class="text-left">
-                <iframe src="http://ghbtns.com/github-btn.html?user=bcit-ci&repo=CodeIgniter&type=watch&count=true&size=large"
-                        allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>
-                <iframe src="http://ghbtns.com/github-btn.html?user=bcit-ci&repo=CodeIgniter&type=fork&count=true&size=large"
-                        allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>
-                <a class="twitter-follow-button"
-                   href="https://twitter.com/CodeIgniter"
-                   data-show-count="true"
-                   data-show-screen-name="false"
-                   data-size="large"
-                   data-lang="en">
-                    @CodeIgniter
+
+            <?php if ( ! empty($github_repo) && is_array($github_repo) && count($github_repo)) : ?>
+            <div class="row text-left">
+              <div class="btn-group">
+                <a href="<?= $github_repo['html_url'] ?>" target="_blank" type="button" class="btn btn-xs btn-white">
+                  <i class="fa fa-lg fa-github"></i>
                 </a>
+                <button type="button" class="btn btn-xs btn-white disabled">
+                  <i class="fa fa-star fa-lg"></i> <?= number_format($github_repo['stargazers_count']) ?>
+                </button>
+                <button type="button" class="btn btn-xs btn-white disabled">
+                  <i class="fa fa-code-fork fa-lg"></i> <?= number_format($github_repo['forks_count']) ?>
+                </button>
+              </div>
+            </div>
+            <?php endif; ?>
+            <div class="row text-right">
+              <a class="twitter-follow-button"
+                 href="https://twitter.com/CodeIgniter"
+                 data-show-count="true"
+                 data-show-screen-name="false"
+                 data-size="large"
+                 data-lang="en">
+                  @CodeIgniter
+              </a>
             </div>
         </div>
     </div>
