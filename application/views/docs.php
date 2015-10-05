f08559ba9a12daa450bd4311f860891f087be1d4
Master Yoda <jim_parry@bcit.ca>
2015-10-05 16:13:27 -0700

    Shorten the view/download button text so it doesn't get cut off at smaller browser/device resolutions. Added back the news & threads headings I accidentally deleted earlier. Signed-off-by:Master Yoda <jim_parry@bcit.ca>

--- application/views/docs.php
+++ application/views/docs.php
@@ -60,8 +60,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<p>CodeIgniter 3.x is the current version of the framework, under active development.</p>
 					<p>CodeIgniter 3.x is licensed under the 
 						<a href="/user_guide/license.html">MIT License</a>.</p>
-					<a href="/user_guide" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-share"></span> Read the User Guide</a>
-					<a href="/data/CodeIgniter-3.0.2-userguide.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download User Guide</a>
+					<a href="/user_guide" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-share"></span> Read the User Guide online</a>
+					<a href="/data/CodeIgniter-3.0.2-userguide.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download the User Guide</a>
 <!-- 					<a href="/data/CodeIgniter3-userguide.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download the User Guide (3.x) to read offline</a>
 					<a href="/data/CodeIgniter3-userguide-epub.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download the User Guide in epub format</a>
  -->
@@ -77,8 +77,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<p>CodeIgniter 2.x is the legacy version of the framework.</p>
 					<p>CodeIgniter 2.x is licensed under the  
 						<a href="/userguide2/license.html">EllisLab open source license</a>.</p>
-					<a href="/userguide2" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-share"></span> Read CI2 User Guide </a>
-					<a href="/data/CodeIgniter2-stable-user_guide.zip" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-save"></span> Download CI2 User Guide</a>
+					<a href="/userguide2" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-share"></span> Read the CodeIgniter 2 User Guide online</a>
+					<a href="/data/CodeIgniter2-stable-user_guide.zip" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-save"></span> Download the CodeIgniter 2 User Guide</a>
 				</div>
 			</div>
 		</div>
--- application/views/download.php
+++ application/views/download.php
@@ -62,11 +62,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					There have been a number of refinements since version 2.x, notably with
 					the database, session handling and encryption. Development of this version is ongoing.<br><br>
 					
-					<a href="https://github.com/bcit-ci/CodeIgniter" class="btn btn-warning btn-block"><span class="glyphicons glyphicons-link"></span>View CI3 on Github</a>
+					<a href="https://github.com/bcit-ci/CodeIgniter" class="btn btn-warning btn-block"><span class="glyphicons glyphicons-link"></span>View CodeIgniter 3 on Github</a>
 					<a href="https://github.com/bcit-ci/CodeIgniter/archive/3.0.2.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download CodeIgniter 3</a>
 					<!--<a href="/data/CodeIgniter-3.0.1-userguide.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download the User Guide (3.0.1) to read offline</a>
 					-->
-					<a href="https://github.com/bcit-ci/codeigniter3-translations/archive/3.0.2.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download Message Translations</a>
+					<a href="https://github.com/bcit-ci/codeigniter3-translations/archive/3.0.2.zip" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-save"></span> Download System Message Translations</a>
 				</div>
 			</div>
 		</div>
@@ -80,7 +80,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					the next major update (2.1.0) came in the fall of 2011, 
 					version (2.2.0) came out in July, 2014, and
 					the current version (2.2.5) came out in October, 2015.<br><br>
-					<a href="https://github.com/bcit-ci/CodeIgniter/tree/2.2-stable" class="btn btn-primary btn-block"><span class="glyphicons glyphicons-link"></span> View CI2 on Github</a>
+					<a href="https://github.com/bcit-ci/CodeIgniter/tree/2.2-stable" class="btn btn-primary btn-block"><span class="glyphicons glyphicons-link"></span> View CodeIgniter 2 on Github</a>
 					<a href="https://github.com/bcit-ci/CodeIgniter/archive/2.2.5.zip" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-save"></span> Download CodeIgniter 2</a>
 				</div>
 			</div>
