8e35b62cc1f5f31538cabd961c75283391cb56ab
Master Yoda <jim_parry@bcit.ca>
2015-10-14 01:53:27 -0700

    Exploit the CI cache to reduce time-limited external API calls. Signed-off-by:Master Yoda <jim_parry@bcit.ca>

--- application/views/theme/_heros.php
+++ application/views/theme/_heros.php
@@ -1,6 +1,6 @@
 {heros}
 <div class="col-md-1">
-	<a href="{url}">
+	<a href="{url}" target="_blank">
 		<img src="{avatar}" width="64" height="64" title="{name}"/></a>
 	<p class="text-center">{stars}</p>
 </div>
