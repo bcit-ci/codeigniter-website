3b242e68f2ef5c1f8ac2c03eaa2efbd8282197dc
Instructor, Computer Systems Technology <jim_parry@bcit.ca>
2016-09-14 19:35:50 -0700

    Update homepage
    Fix the minimum PHP version

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -83,7 +83,7 @@ class Welcome extends Application {
 			array('icon' => 'book', 'label' => 'Clear documentation',
 				'text' => 'The CodeIgniter User Guide comes with the download. It contains an introduction, tutorial, a number of "how to" guides, and then reference documentation for the components that make up the framework.'),
 			array('icon' => 'hdd', 'label' => 'Compatibility with standard hosting',
-				'text' => 'CodeIgniter 3 only needs PHP 5.2.4, and plays nicely with almost all shared or dedicated hosting platforms. Many webapps need a database, and CodeIgniter supports the most common, including MySQL.'),
+				'text' => 'CodeIgniter 3 only needs PHP 5.3.7, and plays nicely with almost all shared or dedicated hosting platforms. Many webapps need a database, and CodeIgniter supports the most common, including MySQL.'),
 			array('icon' => 'road', 'label' => 'No restrictive coding rules',
 				'text' => ' Use your own coding and naming conventions, with only a few caveats that deal with classname conflicts. CodeIgniter looks to empower you, not shackle you.'),
 			array('icon' => 'send', 'label' => 'Simple solutions over complexity',
