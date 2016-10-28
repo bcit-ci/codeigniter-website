87cc2017dbfd718178c5dd3e61bd785f9531d94d
Master Yoda <jim_parry@bcit.ca>
2016-10-28 08:40:02 -0700

    Update for 3.1.2 Signed-off-by:Master Yoda <jim_parry@bcit.ca>

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ $config['footer_choices'] = array(
 	)
 );
 
-$config['stable_version'] = '3.1.1';
+$config['stable_version'] = '3.1.2';
 
 /*
   |--------------------------------------------------------------------------
